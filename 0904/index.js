f41e9e0d6f00aea8c753f3836d9fe40a8060da15
Nagy Máté <nagy.mano.mate@gmail.com>
2025-09-04 12:33:46 +0200

    cadcacadcvavwddwaadwdw

---
+++ 0904/index.js
@@ -0,0 +1,19 @@
+const name = document.getElementById("name").value;
+const city = document.getElementById("city").value;
+const postal = document.getElementById("postal").value;
+const street = document.getElementById("street").value;
+const address = document.getElementById("address").value;
+
+const data = {name,city,postal,street,address};
+
+const btn = document.getElementById("add");
+
+localStorage.clear();
+
+btn.addEventListener("click", () => {
+    console.log(data);
+    if(name != null && city != null && postal != null && street != null && address != null ){
+        localStorage.setItem(`${localStorage.length == undefined ? "0" : localStorage.length}`, data);
+    };
+    console.log(localStorage)
+});
