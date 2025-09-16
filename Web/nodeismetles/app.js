18f4e37956f7582ed4edca01a7cf9a017d7eec4a
Nagy Máté2 <b1nagmat2@vasvari.org>
2025-09-16 09:39:40 +0200

    node ismetles

---
+++ Web/nodeismetles/app.js
@@ -0,0 +1,58 @@
+import express from "express";
+
+const PORT = 3000;
+const app = express();
+
+app.use(express.json());
+
+const users = [
+    {id: 1, name: 'Alex', age: 19},
+    {id: 2, name: 'Dave', age: 20},
+    {id: 3, name: 'Michael', age: 21},
+    {id: 4, name: 'Lana', age: 22},
+]
+
+app.get("/", (req, res) => {
+    res.status(200).json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+    const id = +req.params.id;
+
+    const user = users.find(x => x.id === id);
+    if(!user){
+        return res.status(404).json({message:"User not found!"});
+    }
+    res.status(200).json(user);
+});
+
+app.post("/users", (req, res) => {
+    const {name, age} = req.body;
+
+    if(!name || !age){
+        return res.status(400).json({message:"Invalid credentials!"});
+    }
+
+    const id = users.lenght ? users[users.length - 1].id + 1 : 1;
+    const user = {id, name, age};
+
+    users.push(user);
+
+    res.status(201).json(user);
+});
+
+app.delete("/users/:id", (req, res) =>{
+    const id = +req.params.id;
+
+    const user = users.find(x => x.id === id);
+    if(!user){
+        return res.status(404).json({message:"User not found!"});
+    }
+    const index = users.indexOf(user);
+    users.splice(index, 1);
+    res.status(200).json({message:"Deleted successfully!"});
+})
+
+app.listen(PORT, () => {
+    console.log(`Server runs on http://localhost:${PORT}`)
+});
